b4b015966fdd3620c1fb2b6bdab121d551a3bdbd
AaravJayAnand <112331610+AaravJayAnand@users.noreply.github.com>
2026-06-22 17:44:45 -0400

    Transitioned to NumPy
    - rip "for loops" since they're slow
    - added a sentence in README
    
    for loops may come back since networks might need them (coming soon btw)

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# Research-Project
+# SIR Model for Disease Spread
+
+An SIR model simulation created in Python using NumPy (for calculations), Pandas (for handling data), and Matplotlib (for a graphical visualization)
+
+wip
--- main.py
+++ main.py
@@ -1,65 +1,40 @@
-import random as rd
 import matplotlib.pyplot as plt
 import pandas as pd
+import numpy as np
 
 PUBLIC_CONTACT_RATE = 0.4
 SI_RATE = 0.5
 IR_RATE = 0.071
-POPULATION = 20000
-INITIAL_INFECTED = 200
-
-class Individual:
-    def __init__(self, simulation, initial_state=0):
-        self.state = initial_state
-        self.simulation = simulation
-    
-    def update(self):
-        if self.state == 0:
-            if rd.random() < PUBLIC_CONTACT_RATE * SI_RATE * self.simulation.I / POPULATION:
-                self.state += 1
-        elif self.state == 1:
-            if rd.random() < IR_RATE:
-                self.state += 1
+POPULATION = 20000000
+INITIAL_INFECTED = 2000
 
 class Simulation:
-    def __init__(self, individuals: int):
-        self.S = POPULATION - INITIAL_INFECTED
-        self.I = INITIAL_INFECTED
-        self.R = 0
-        self.population = []
-        for _ in range(self.I):
-            self.population.append(Individual(self, 1))
-        for _ in range(individuals - self.I):
-            self.population.append(Individual(self))
-    
+    def __init__(self):
+        self.individuals = np.zeros(POPULATION)
+        self.individuals[0:INITIAL_INFECTED] = 1
+        self.recovered = 0
+        self.tick = 0
+        self.data = {"day": [], "S": [], "I": [], "R": []}
+
     def update(self):
-        new_S = 0
-        new_I = 0
-        new_R = 0
-        for individual in self.population:
-            individual.update()
-            if individual.state == 0:
-                new_S += 1
-            if individual.state == 1:
-                new_I += 1
-            if individual.state == 2:
-                new_R += 1
-        self.S = new_S
-        self.I = new_I
-        self.R = new_R
+        SI_mask = (self.individuals == 0) & (np.random.random(POPULATION) < PUBLIC_CONTACT_RATE * SI_RATE * np.sum(self.individuals == 1) / POPULATION)
+        IR_mask = (self.individuals == 1) & (np.random.random(POPULATION) < IR_RATE)
+        
+        self.individuals[SI_mask] = 1
+        self.individuals[IR_mask] = 2
+        
+        self.recovered = int(np.sum(self.individuals == 2))
+        self.data["day"].append(self.tick)
+        self.data["S"].append(int(np.sum(self.individuals == 0)))
+        self.data["I"].append(int(np.sum(self.individuals == 1)))
+        self.data["R"].append(self.recovered)
+        self.tick += 1
 
-simulation = Simulation(POPULATION)
-data = {"day": [], "S": [], "I": [], "R": []}
-
-tick = 0
-print("...")
-while simulation.I > 0:
+simulation = Simulation()
+simulation.update()
+while simulation.data["I"][-1] > 0:
     simulation.update()
-    data["day"].append(tick)
-    data["S"].append(simulation.S)
-    data["I"].append(simulation.I)
-    data["R"].append(simulation.R)
-    tick += 1
+data = simulation.data
 
 plt.style.use("dark_background")
 plt.plot(data["day"], data["S"], color=(1, 1, 0), label="Susceptible", lw=2)
@@ -67,8 +42,8 @@ plt.plot(data["day"], data["I"], color=(1, 0, 0), label="Infected", lw=2)
 plt.plot(data["day"], data["R"], color=(0, 1, 0), label="Recovered", lw=2)
 plt.legend()
 plt.title(f"SIR model of COVID-19 given a population of {POPULATION} with {INITIAL_INFECTED} initially infected")
-plt.xlabel(f"Days ({tick})")
-plt.ylabel(f"Number of Individuals (S: {simulation.S}, I: {simulation.I}, R: {simulation.R})")
+plt.xlabel(f"Days ({data['day']})")
+plt.ylabel(f"Number of Individuals (S: {data["S"]}, I: {data["I"]}, R: {data["R"]})")
 
 df = pd.DataFrame(data)
 print(df.tail(100))
